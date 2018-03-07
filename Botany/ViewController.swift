b35cb1cc52afb4d651642aae3dba71ad6fe74676
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-03-07 12:05:20 -0800

    initial

--- Botany/ViewController.swift
+++ Botany/ViewController.swift
@@ -7,19 +7,91 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    let flower = Flower()
+    
+    let imagePicker = UIImagePickerController()
+    
+    @IBOutlet var previewImage: UIImageView!
+    @IBOutlet var predictionLabel: UILabel!
+    @IBOutlet var probability: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imagePicker.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func selectPhoto() {
+        let ac = UIAlertController(title: "Select Photo", message: nil, preferredStyle: .actionSheet)
+        
+        let camera = UIAlertAction(title: "Camera", style: .default) { (alert) in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        let photoLibrary = UIAlertAction(title: "Photo Library", style: .default) { (alert) in
+            
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        
+        ac.addAction(camera)
+        ac.addAction(photoLibrary)
+        ac.addAction(cancel)
+        
+        self.present(ac, animated: true, completion: nil)
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.previewImage.image = image
+        classify(image: image)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func classify(image: UIImage) {
+        let buffer = image.buffer(with: CGSize(width:224, height:224))
+        let prediction = try? flower.prediction(image: buffer!)
+        
+        predictionLabel.text = prediction!.flowerType
+        probability.text = "\(prediction!.flowerTypeProbability)"
+        
+    }
+    
+    
 
 
+}
+
+extension UIImage {
+    func buffer(with size:CGSize) -> CVPixelBuffer? {
+        if let image = self.cgImage {
+            let frameSize = size
+            var pixelBuffer:CVPixelBuffer? = nil
+            let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
+            if status != kCVReturnSuccess {
+                return nil
+            }
+            CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
+            let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
+            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+            let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
+            let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
+            context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
+            CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+            
+            return pixelBuffer
+        }else{
+            return nil
+        }
+    }
 }
 
