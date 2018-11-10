4a80a7dbb7d4a05e03d7af8aa2a6a6c1993d50d9
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-11-09 20:07:34 -0800

    middle of changes

---
+++ Botany/BotanyView.swift
@@ -0,0 +1,22 @@
+//
+//  BotanyView.swift
+//  Botany
+//
+//  Created by Cyril Garcia on 11/9/18.
+//  Copyright © 2018 By Cyril. All rights reserved.
+//
+
+import UIKit
+
+class BotanyView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
--- Botany/ViewController.swift
+++ Botany/ViewController.swift
@@ -7,99 +7,20 @@
 //
 
 import UIKit
-import CoreML
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, FlowersDelegate {
 
-    let flower = Flower()
-    let plants = Oxford102()
-    
-    let imagePicker = UIImagePickerController()
-    
-    @IBOutlet var previewImage: UIImageView!
-    @IBOutlet var predictionLabel: UILabel!
-    @IBOutlet var probability: UILabel!
+    let model = Flowers(Flower().model)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        imagePicker.delegate = self
+        
         
     }
     
-    @IBAction func selectPhoto() {
-        let ac = UIAlertController(title: "Select Photo", message: nil, preferredStyle: .actionSheet)
-        
-        let camera = UIAlertAction(title: "Camera", style: .default) { (alert) in
-            self.imagePicker.sourceType = .camera
-            self.present(self.imagePicker, animated: true, completion: nil)
-        }
-        
-        let photoLibrary = UIAlertAction(title: "Photo Library", style: .default) { (alert) in
-            
-            self.imagePicker.sourceType = .photoLibrary
-            self.present(self.imagePicker, animated: true, completion: nil)
-        }
-        
-        let cancel = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
-        
-        ac.addAction(camera)
-        ac.addAction(photoLibrary)
-        ac.addAction(cancel)
-        
-        self.present(ac, animated: true, completion: nil)
-    }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func flowerPrediction(_ name: String, _ confidenceLevel: Double) {
         
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        self.previewImage.image = image
-
-        classify(image: image)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func classify(image: UIImage) {
-        
-        let buffer = image.buffer(with: CGSize(width:224, height:224))
-        let prediction = try? flower.prediction(image: buffer!)
-        
-        predictionLabel.text = prediction!.flowerType
-        probability.text = "\(prediction!.flowerTypeProbability)"
-        
-    }
-    
-    func classify(plant: UIImage) {
-        let buffer = plant.buffer(with: CGSize(width: 227, height: 227))
-        let prediction = try? plants.prediction(data: buffer!)
-        
-        predictionLabel.text = prediction!.classLabel
-        probability.text = "\(prediction!.featureNames)"
-    }
-    
-}
-
-extension UIImage {
-    func buffer(with size:CGSize) -> CVPixelBuffer? {
-        if let image = self.cgImage {
-            let frameSize = size
-            var pixelBuffer:CVPixelBuffer? = nil
-            let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(frameSize.width), Int(frameSize.height), kCVPixelFormatType_32BGRA , nil, &pixelBuffer)
-            if status != kCVReturnSuccess {
-                return nil
-            }
-            CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags.init(rawValue: 0))
-            let data = CVPixelBufferGetBaseAddress(pixelBuffer!)
-            let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-            let bitmapInfo = CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Little.rawValue | CGImageAlphaInfo.premultipliedFirst.rawValue)
-            let context = CGContext(data: data, width: Int(frameSize.width), height: Int(frameSize.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: bitmapInfo.rawValue)
-            context?.draw(image, in: CGRect(x: 0, y: 0, width: image.width, height: image.height))
-            CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
-            
-            return pixelBuffer
-        }else{
-            return nil
-        }
     }
 }
 
